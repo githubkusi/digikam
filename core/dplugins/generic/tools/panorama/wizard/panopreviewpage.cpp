03bb5ec7a6ff6308084bad94fe9576d29c17289e
Maik Qualmann <metzpinguin@gmail.com>
2019-08-16 21:05:15 +0200

    try it here with a process event CCBUGS: 410973

--- core/dplugins/generic/tools/panorama/wizard/panopreviewpage.cpp
+++ core/dplugins/generic/tools/panorama/wizard/panopreviewpage.cpp
@@ -30,6 +30,7 @@
 #include <QMutexLocker>
 #include <QTextDocument>
 #include <QStandardPaths>
+#include <QApplication>
 #include <QVBoxLayout>
 
 // KDE includes
@@ -231,6 +232,8 @@ void PanoPreviewPage::startStitching()
     d->progressBar->progressThumbnailChanged(QIcon::fromTheme(QLatin1String("panorama")).pixmap(22, 22));
     d->progressBar->show();
     d->postProcessing->show();
+
+    qApp->processEvents();
 
     d->mngr->resetPanoPto();
     d->mngr->resetMkUrl();
