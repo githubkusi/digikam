eae0a0411c865204c62845a50ce4b702b347e49a
Gilles Caulier <caulier.gilles@gmail.com>
2019-09-03 18:56:36 +0200

    polish

--- core/DigikamExportAPI.cmake
+++ core/DigikamExportAPI.cmake
@@ -15,79 +15,85 @@
 
 install(FILES
 
-            ${CMAKE_CURRENT_BINARY_DIR}/app/utils/digikam_config.h
-            ${CMAKE_CURRENT_BINARY_DIR}/app/utils/digikam_core_export.h
-            ${CMAKE_CURRENT_BINARY_DIR}/app/utils/digikam_database_export.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/app/utils/digikam_export.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/app/utils/digikam_globals.h
+               # As part of DigikamCore
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dplugin.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dpluginaction.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dpluginauthor.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dplugineditor.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dplugingeneric.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/setup/dpluginloader.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/iface/dinfointerface.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/iface/dmetainfoiface.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/ditemslist.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dplugindialog.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dpreviewimage.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dpreviewmanager.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dsavesettingswidget.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dwizarddlg.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dwizardpage.h
+              ${CMAKE_CURRENT_BINARY_DIR}/app/utils/digikam_config.h
+              ${CMAKE_CURRENT_BINARY_DIR}/app/utils/digikam_core_export.h
+              ${CMAKE_CURRENT_BINARY_DIR}/app/utils/digikam_database_export.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/app/utils/digikam_export.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/app/utils/digikam_globals.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/items/gpsitemcontainer.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/correlator/gpsdatacontainer.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/core/geoifacetypes.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/core/geocoordinates.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/reversegeocoding/rginfo.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dplugin.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dpluginaction.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dpluginauthor.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dplugineditor.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/core/dplugingeneric.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/setup/dpluginloader.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/iface/dinfointerface.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/iface/dmetainfoiface.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/ditemslist.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dplugindialog.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dpreviewimage.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dpreviewmanager.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dsavesettingswidget.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dwizarddlg.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dplugins/widgets/dwizardpage.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/queuemanager/dplugins/dpluginbqm.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/queuemanager/manager/batchtool.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/queuemanager/manager/queuesettings.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/items/gpsitemcontainer.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/correlator/gpsdatacontainer.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/core/geoifacetypes.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/core/geocoordinates.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/geolocation/geoiface/reversegeocoding/rginfo.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/imageeditor/editor/imageiface.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/utilities/imageeditor/core/iofilesettings.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/imageeditor/editor/imageiface.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/imageeditor/core/iofilesettings.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dimg.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolor.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolorpixelaccess.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolorcomposer.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolorblend.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dshareddata.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/drawdecoding.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/history/historyimageid.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/history/filteraction.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/icc/iccprofile.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/bcg/bcgcontainer.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/wb/wbcontainer.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/curves/curvescontainer.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dimg.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolor.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolorpixelaccess.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolorcomposer.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dcolorblend.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/dshareddata.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/drawdecoding.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/history/historyimageid.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/history/filteraction.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/icc/iccprofile.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/bcg/bcgcontainer.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/wb/wbcontainer.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dimg/filters/curves/curvescontainer.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/metadataengine/engine/metaengine.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/metadataengine/engine/metaengine_data.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/metadataengine/containers/photoinfocontainer.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/metadataengine/engine/metaengine.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/metadataengine/engine/metaengine_data.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/metadataengine/containers/photoinfocontainer.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/rawengine/drawdecoder.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/rawengine/drawinfo.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/rawengine/drawdecodersettings.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/rawengine/drawdecoder.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/rawengine/drawinfo.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/rawengine/drawdecodersettings.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/item/containers/iteminfo.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/item/containers/iteminfolist.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbfields.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbconstants.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbalbuminfo.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbaccess.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredburl.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineparameters.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineaction.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineconfig.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineerrorhandler.h
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineconfigsettings.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/widgets/files/filesaveconflictbox.h
 
-            ${CMAKE_CURRENT_SOURCE_DIR}/libs/widgets/files/filesaveconflictbox.h
+              # As part of DigikamDatabase
+
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/item/containers/iteminfo.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/item/containers/iteminfolist.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbfields.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbconstants.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbalbuminfo.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredbaccess.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/coredb/coredburl.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineparameters.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineaction.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineconfig.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineerrorhandler.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/database/engine/dbengineconfigsettings.h
+
+              # As part of DigikamGui
+
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/queuemanager/dplugins/dpluginbqm.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/queuemanager/manager/batchtool.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/utilities/queuemanager/manager/queuesettings.h
 
         DESTINATION
 
-            ${CMAKE_INSTALL_INCLUDEDIR}/digikam
+              ${CMAKE_INSTALL_INCLUDEDIR}/digikam
 )
