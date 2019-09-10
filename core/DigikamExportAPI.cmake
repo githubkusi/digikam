2011a38c8c638cce763fbf9aa91af2b01775e9ae
Gilles Caulier <caulier.gilles@gmail.com>
2019-09-10 17:34:21 +0200

    typo

--- core/DigikamExportAPI.cmake
+++ core/DigikamExportAPI.cmake
@@ -91,7 +91,7 @@ install(FILES
               ${CMAKE_CURRENT_SOURCE_DIR}/libs/threadimageio/thumb/thumbnailinfo.h
 
               ${CMAKE_CURRENT_SOURCE_DIR}/libs/dialogs/dmessagebox.h
-              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dialogs/dprogresdlg.h
+              ${CMAKE_CURRENT_SOURCE_DIR}/libs/dialogs/dprogressdlg.h
 
               # As part of DigikamDatabase
 
