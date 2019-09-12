8bd94ff224d61f9c797eeb48d1833bef42b8c1a1
Gilles Caulier <caulier.gilles@gmail.com>
2019-09-12 04:20:43 +0200

    revert

--- project/bundles/macports/01-build-macports.sh
+++ project/bundles/macports/01-build-macports.sh
@@ -290,7 +290,8 @@ cmake --build . --config RelWithDebInfo --target ext_opencv      -- -j$CPU_CORES
 cmake --build . --config RelWithDebInfo --target ext_exiv2       -- -j$CPU_CORES
 cmake --build . --config RelWithDebInfo --target ext_qtav        -- -j$CPU_CORES
 cmake --build . --config RelWithDebInfo --target ext_heif        -- -j$CPU_CORES
-cmake --build . --config RelWithDebInfo --target ext_imagemagick -- -j$CPU_CORES
+
+port install ImageMagick
 
 #################################################################################################
 
