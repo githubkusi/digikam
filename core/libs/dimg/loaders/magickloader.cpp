642394b9987b76bda73fc80ec3019e217ace0917
Maik Qualmann <metzpinguin@gmail.com>
2019-09-16 19:50:52 +0200

    debug as warning

--- core/libs/dimg/loaders/magickloader.cpp
+++ core/libs/dimg/loaders/magickloader.cpp
@@ -83,7 +83,7 @@ bool MagickLoader::load(const QString& filePath, DImgLoaderObserver* const obser
         mimeType.startsWith(QLatin1String("video/")) ||
         mimeType.startsWith(QLatin1String("audio/")))
     {
-        qCDebug(DIGIKAM_DIMG_LOG) << "This mime type is on the blacklist:" << mimeType;
+        qCWarning(DIGIKAM_DIMG_LOG) << "This mime type is on the blacklist:" << mimeType;
         loadingFailed();
         return false;
     }
