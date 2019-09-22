027d45ea481b9bb27dd617e8d2faad7d8eade41c
Gilles Caulier <caulier.gilles@gmail.com>
2019-09-22 20:35:13 +0200

    polish

--- core/dplugins/dimg/qimage/dimgqimageplugin.cpp
+++ core/dplugins/dimg/qimage/dimgqimageplugin.cpp
@@ -8,9 +8,6 @@
  *
  * Copyright (C) 2019      by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
- * Lua script inspired from Darktable Gimp plugin
- * Copyright (C) 2015-2017 by Tobias Ellinghaus
- *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
