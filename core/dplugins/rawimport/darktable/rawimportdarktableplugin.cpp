93eb5ff71ee8438142407a5d1bc15be995310181
Gilles Caulier <caulier.gilles@gmail.com>
2019-09-22 20:34:31 +0200

    krazy++

--- core/dplugins/rawimport/darktable/rawimportdarktableplugin.cpp
+++ core/dplugins/rawimport/darktable/rawimportdarktableplugin.cpp
@@ -8,8 +8,7 @@
  *
  * Copyright (C) 2019      by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
- * Lua script inspired from Darktable Gimp plugin
- * Copyright (C) 2015-2017 by Tobias Ellinghaus
+ * Lua script inspired from Darktable Gimp plugin by Tobias Ellinghaus
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
